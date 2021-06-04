02c1212bdc2eacf31df34002fe6766481587b052
Maddie Waldie <maddiewaldie@me.com>
2021-06-04 19:46:39 -0400

    finished part 1

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maddie on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
